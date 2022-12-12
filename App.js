9619b7d2d5d90590447fa2538972dc66bb5ee962
Kris-Haskin <viniferouskris@gmail.com>
2022-12-11 19:57:12 -0600

    First commit

--- App.js
+++ App.js
@@ -1,52 +1,137 @@
-import React, { useState } from 'react';
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState, useEffect, Component } from 'react';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView, SafeAreaView, Alert } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import * as SQLite from "expo-sqlite";
 import emptyTubes from './assets/full-tp-rolls.png';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as SplashScreen from 'expo-splash-screen';
+import * as WebBrowser from 'expo-web-browser';
 
 SplashScreen.preventAutoHideAsync();
-setTimeout(SplashScreen.hideAsync, 1000);
-
-function HomeScreen({ navigation }) {
-  const [numberOfRolls, setNumberOfRolls] = useState(1);
-  const [sheetsPerRoll, setsheetsPerRoll] = useState(1);
-  const [price, setPrice] = useState(1);
-  const [sqFtPerRoll, setSqFtPerRoll] = useState(1);
-  const [brand, setBrand] = useState(1);
-
-  const [pricePerRoll, setPricePerRoll] = useState(1);
-  const [pricePerSqFt, setpricePerSqFt] = useState(1);
-  const [pricePerSheet, setPricePerSheet] = useState(1);
+setTimeout(SplashScreen.hideAsync, 2000);
 
 
-  const onPress = () => {
-// calculate()
 
-
-    navigation.navigate('Details', {
-      numberOfRolls: numberOfRolls,
-      sheetsPerRoll: sheetsPerRoll,
-      price: price,
-      sqFtPerRoll: sqFtPerRoll,
-      brand: brand,
-
-      pricePerRoll: pricePerRoll,
-      pricePerSqFt: pricePerSqFt,
-      pricePerSheet: pricePerSheet,
-    });
+function openDatabase() {
+  if (Platform.OS === "web") {
+    return {
+      transaction: () => {
+        return {
+          executeSql: () => { },
+        };
+      },
+    };
   }
 
-const calculate = () => {
-  const PPR = (price/numberOfRolls).toFixed(2);
-  setPricePerRoll(PPR)
+  const db = SQLite.openDatabase("TPDBnumberTwo.db");
 
-  const PPSF = ((price/numberOfRolls)/sqFtPerRoll).toFixed(5);
-  setpricePerSqFt(PPSF)
-
-  const PPS = ((price/numberOfRolls)/sheetsPerRoll).toFixed(5);
-  setPricePerSheet(PPS)
+  return db;
 }
+
+const db = openDatabase();
+
+//function totals() {
+function Totals() {
+  // const [totals, settotals] = useState(null);  
+  const [totals, setTotals] = useState(null);
+
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `select id, brand, numberOfRolls, sheetsPerRoll, price, sqFtPerRoll, pricePerRoll, pricePerSqFt, pricePerSheet from totals order by pricePerSqFt;`,
+        [],
+        (_, { rows: { _array } }) => setTotals(_array)
+      );
+    });
+  },[]);
+
+  if (totals === null || totals.length === 0) {
+    return null;
+  }
+
+  return (
+    <View style={styles.sectionContainer}>
+
+      {/* <Text style={styles.title} >Totals</Text> */}
+
+      {totals.map(({ id, brand, numberOfRolls, sheetsPerRoll, price, sqFtPerRoll, pricePerRoll, pricePerSqFt, pricePerSheet }) => (
+        <Text style={styles.map} key={id}>{brand}:{numberOfRolls}-pack  {("\n")}costs {pricePerRoll} per roll. {"\n"}
+          That is {pricePerSheet} per sheet{"\n"}or {pricePerSqFt} per square foot </Text>
+
+      ))}
+    </View>
+
+
+
+  );
+}
+
+
+function HomeScreen({ navigation }) {
+  const [numberOfRolls, setNumberOfRolls] = useState("");
+  const [sheetsPerRoll, setsheetsPerRoll] = useState("");
+  const [price, setPrice] = useState("");
+  const [sqFtPerRoll, setSqFtPerRoll] = useState("");
+  const [brand, setBrand] = useState("");
+
+  const [pricePerRoll, setPricePerRoll] = useState("");
+  const [pricePerSqFt, setpricePerSqFt] = useState("");
+  const [pricePerSheet, setPricePerSheet] = useState("");
+
+  //////////////////////////////////////////////////////////
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "create table if not exists totals (id integer primary key not null, brand real, numberOfRolls real, sheetsPerRoll real, price real, sqFtPerRoll real, pricePerRoll real, pricePerSqFt real, pricePerSheet real);"
+      );
+    });
+  }, []);
+  //////////////////////////////////////////////////////////
+
+  const addTotals = () => {
+    if (brand === null || brand === "" || price === null || price === "" || numberOfRolls === null || numberOfRolls === "" ||
+      sheetsPerRoll === null || sheetsPerRoll === "" || sqFtPerRoll === null || sqFtPerRoll === "") {
+      Alert.alert('Data Validation Error', 'Please enter full and proper data.');
+      return false;
+    }
+    
+    const Totals = CalculateTotals();
+    if (Totals === null || Totals === "") {
+      return false;
+    }
+    
+CalculateTotals();
+
+    db.transaction(
+      (tx) => {
+        tx.executeSql("insert into totals (brand, numberOfRolls, sheetsPerRoll, price, sqFtPerRoll, pricePerRoll, pricePerSqFt, pricePerSheet) values (?, ?, ?, ?, ?, ?, ?, ?)", [brand, numberOfRolls, sheetsPerRoll, price, sqFtPerRoll, pricePerRoll, pricePerSqFt, pricePerSheet]);
+        tx.executeSql("select * from totals", [], (_, { rows }) =>
+          console.log(JSON.stringify(rows))
+        );
+      }
+    );
+
+    navigation.navigate('Details', {
+
+    });
+
+
+
+  }
+
+  const CalculateTotals = () => {
+    
+    const PPR = (price / numberOfRolls).toFixed(2);
+    const PPSF = ((price / numberOfRolls) / sqFtPerRoll).toFixed(5);
+    const PPS = ((price / numberOfRolls) / sheetsPerRoll).toFixed(5);
+
+    setPricePerRoll(PPR);
+    setpricePerSqFt(PPSF);
+    setPricePerSheet(PPS);
+    return;
+  }
 
 
   return (
@@ -55,7 +140,7 @@ const calculate = () => {
       <Text style={styles.title}>Brand:</Text>
       <TextInput
         style={styles.TxtInput}
-        placeholder="Brand Name"
+        placeholder="Enter Brand Name"
         onChangeText={value => setBrand(value)}
       />
 
@@ -76,25 +161,24 @@ const calculate = () => {
       <Text style={styles.title}>Sheets per roll:</Text>
       <TextInput
         style={styles.TxtInput}
-        placeholder="Enter the Number of Sheets per Roll"
+        placeholder="Number of Sheets per Roll"
         onChangeText={value => setsheetsPerRoll(value)}
       />
       <Text style={styles.title}>Sq feet per roll:</Text>
 
       <TextInput
         style={styles.TxtInput}
-        placeholder="Enter the Number of square feet per Roll"
+        placeholder="Number of square feet per Roll"
         onChangeText={value => setSqFtPerRoll(value)}
       />
 
       <TouchableOpacity
         style={styles.button}
-        onPress={onPress}
+        onPress={addTotals}
 
       >
         <Text style={styles.buttonText}>Calculate</Text>
       </TouchableOpacity>
-
 
     </View>
   );
@@ -102,21 +186,43 @@ const calculate = () => {
 
 function ResultsScreen({ route }) {
   /* 2. Get the param */
-  const { numberOfRolls, sheetsPerRoll, price, sqFtPerRoll, brand} = route.params;
- 
-  const PPR = (price/numberOfRolls).toFixed(2);
-  const PPSF = ((price/numberOfRolls)/sqFtPerRoll).toFixed(5);
-  const PPS = ((price/numberOfRolls)/sheetsPerRoll).toFixed(5);
-   
+  // const { numberOfRolls, sheetsPerRoll, price, sqFtPerRoll, brand } = route.params;
+
+  // const PPR = (price / numberOfRolls).toFixed(2);
+  // const PPSF = ((price / numberOfRolls) / sqFtPerRoll).toFixed(5);
+  // const PPS = ((price / numberOfRolls) / sheetsPerRoll).toFixed(5);
+  const handleButtonPress = () => { WebBrowser.openBrowserAsync('https://www.sunrisespecialty.com/how-to-use-toilet-paper'); }
   return (
-    <View style={styles.details}>
-            <Text style={styles.title}>{brand}:</Text>
-      <Text style={styles.TPStats}>{numberOfRolls}-pack costs ${PPR} per roll:</Text>
-      <Text style={styles.TPStats}>Thats is {PPSF} per square foot</Text>
-      <Text style={styles.TPStats}>or {PPS} per sheet</Text>
+
+    <SafeAreaView style={styles.container}>
+
+      {Platform.OS === "web" ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text style={styles.heading}>
+            Expo SQlite is not supported on web!
+          </Text>
+        </View>
+      ) : (
+        <ScrollView style={styles.scrollContainer}>
+
+
+          <Totals />
+
+        </ScrollView>
+
+      )}
       <Image source={emptyTubes} style={styles.picture} />
 
-    </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleButtonPress}
+      // https://www.sunrisespecialty.com/how-to-use-toilet-paper
+      >
+        <Text style={styles.buttonText}>click here for more infomation {("\n")} on effecient use of toilet paper</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 const Stack = createNativeStackNavigator();
@@ -130,6 +236,7 @@ export default function App() {
             backgroundColor: '#4dbdea',
           },
           headerTintColor: '#fff',
+
         }}>
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Toilet Paper Cost Calculator" }} />
         <Stack.Screen name="Details" component={ResultsScreen
@@ -145,14 +252,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-   // alignItems: 'left',
-   marginLeft: 10,
-   justifyContent: 'center',
+    justifyContent: 'center',
 
   },
   details: {
     flex: 1,
-   justifyContent: 'center',
+    justifyContent: 'center',
   },
   TPStats: {
     fontSize: 20,
@@ -161,8 +266,9 @@ const styles = StyleSheet.create({
   },
 
   picture: {
-    witdh: 305,
+    witdh: 280,
     Height: 159,
+    paddingBottom: 20,
   },
 
   subtitle: {
@@ -174,7 +280,7 @@ const styles = StyleSheet.create({
   },
 
   title: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: "bold",
     marginLeft: 10,
     marginTop: 5,
@@ -182,15 +288,17 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    backgroundColor: "gray",
+    backgroundColor: '#4dbdea',
     padding: 10,
-    marginBottom: 20,
+    marginTop: 20,
+    width: 420,
 
   },
 
   buttonText: {
     fontSize: 20,
     color: "white",
+    textAlign: 'center',
   },
 
   TxtInput: {
@@ -198,11 +306,20 @@ const styles = StyleSheet.create({
     width: 200,
     marginBottom: 5,
     marginTop: 5,
-  
+    fontSize: 15,
+
   },
-  txtOutput: {
+  sectionContainer: {
+    marginBottom: 16,
+    marginHorizontal: 16,
+  },
+  map: {
+    marginBottom: 5,
     fontSize: 20,
-  }
+    marginLeft: 15,
+
+  },
+
 
 });
 
